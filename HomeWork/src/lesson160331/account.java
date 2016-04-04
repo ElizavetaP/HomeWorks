30dad5640f78eddeecbac691483cacf3669a945b
Liza <lizap@bk.ru>
2016-04-04 16:22:33 +0300

    with salary

--- HomeWork/src/lesson160331/account.java
+++ HomeWork/src/lesson160331/account.java
@@ -3,8 +3,9 @@ package lesson160331;
 public class account {
 
 	public static void main(String[] args) {
-		
-		double a = 55.0 - 55*0.13;
+
+		int s = 55000;
+		double a = s - s * 0.13;
 		System.out.println(a);
 
 	}
