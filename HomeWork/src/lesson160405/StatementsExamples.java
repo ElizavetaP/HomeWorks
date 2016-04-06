c0313c630582dc55bf607afd8032459f84047a82
Liza <lizap@bk.ru>
2016-04-06 17:35:17 +0300

    classwork

---
+++ HomeWork/src/lesson160405/StatementsExamples.java
@@ -0,0 +1,29 @@
+package lesson160405;
+
+import java.util.Arrays;
+
+public class StatementsExamples {
+
+	public static void main(String[] args) {
+
+		if (args.length == 0) {
+			System.out.println("Usage: EvenOddExample <number>");
+			System.exit(0);
+		}
+
+		int x = Integer.parseInt(args[0]);
+
+		boolean r = (x >= 10) & (x <= 20);
+
+		if (r == true) {
+			System.out.println("Popal");
+
+		} 
+
+		if (r == false) {
+			System.out.println("Mimo");
+		}
+
+	}
+
+}
