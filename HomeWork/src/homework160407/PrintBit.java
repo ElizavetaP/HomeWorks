2dae4bdf842896f9b742984c93f09cedec43293d
Liza <lizap@bk.ru>
2016-04-08 14:22:47 +0300

    print bit

---
+++ HomeWork/src/homework160407/PrintBit.java
@@ -0,0 +1,34 @@
+package homework160407;
+
+public class PrintBit {
+
+	public static void main(String[] args) {
+
+		if (args == null) {
+			System.exit(1);
+		}
+
+		if (args.length == 0) {
+			System.out.println("Usage:  PrintBit <number>");
+			System.exit(0);
+		}
+
+		int x = Integer.parseInt(args[0]);
+		
+		System.out.println(Integer.toBinaryString(x));
+
+		while (x > 0) {
+
+			if (x % 2 == 0) {
+				System.out.println(0);
+			} else {
+				System.out.println(1);
+			}
+
+			x = x >> 1;
+
+		}
+
+	}
+
+}
