ccb5e6977e7f1155884c068d41b8cd1aa35d25b6
test <test@localhost>
2016-05-04 21:11:23 +0300

    Max Vector

---
+++ HomeWork/src/homework160428/MaxVector.java
@@ -0,0 +1,32 @@
+package homework160428;
+
+import java.util.Arrays;
+
+public class MaxVector {
+
+	public static void main(String[] args) {
+		
+		int[][] a = {
+				{0, 90, 8},
+				{7, 4,100},
+				{8,1,2,8,9},
+				{9,9,7,50},
+		};
+		
+		System.out.println(Arrays.toString(maxVector(a)));
+		
+	}
+	
+	static int[] maxVector(int[][] a){
+		int[] vector = new int[a.length];
+		for (int i = 0; i < a.length; i++) {
+			int max = 0;
+			for (int num : a[i]) {
+				max = Math.max(max, num);
+			}
+			vector[i] = max;
+		}
+		return vector;
+	}
+
+}
