a814d78cf65b386e36013b6dfab2562f6448b32e
test <test@localhost>
2016-05-04 19:57:41 +0300

    BinarySearch with item

---
+++ HomeWork/src/homework160428/BinarySearch.java
@@ -0,0 +1,43 @@
+package homework160428;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+
+		int[] a = { 1, 8, 4, 3, 7, 6, 9, 50, 3 };
+
+		Arrays.sort(a);
+
+		System.out.println(Arrays.toString(a));
+
+		System.out.println(binarySearch(a, 5));
+
+	}
+
+	static int binarySearch(int[] a, int key) {
+
+		int left = 0, right = a.length - 1;
+		int mid = 1;
+
+		while (right >= left) {
+			mid = left + (right - left) / 2;
+			if (key == a[mid]) {
+				return mid;
+			}
+			if (key > a[mid]) {
+				left = mid + 1;
+			} else if (key < a[mid]) {
+				right = mid - 1;
+			}
+
+		}
+		if (key > a[mid]) {
+			mid++;
+		}
+		return -1 - 1 * mid;
+
+	}
+
+}
