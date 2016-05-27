06a01e0121779a96e919d4cd4c320a6cc9512efa
Elizaveta <Elizaveta@Liza-pc.Dlink>
2016-05-27 12:36:31 +0300

    start Series

--- HomeWork/src/homework160526/Series.java
+++ HomeWork/src/homework160526/Series.java
@@ -4,15 +4,13 @@ public class Series {
 	public static void main(String[] args) {
 		double a = 1.0;
 		for (int n = 1; n < Integer.MAX_VALUE; n++) {
-			a = 1.0/((n+1)*(n+1));
-			if (a<0){
+			a = 1.0 / ((n + 1) * (n + 1));
+			if (a < 0) {
+				System.out.println(n - 1);
 				break;
 			}
-			if(a <0.000001){
-				System.out.println(a);
-				System.out.println(n);
-				
-			}
+			System.out.println(a);
+
 		}
 	}
 
