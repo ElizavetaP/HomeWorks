6245077df3fabf3866796024fb0ab521330986db
Elizaveta <Elizaveta@Liza-pc.Dlink>
2016-05-29 13:13:58 +0300

    with method

--- HomeWork/src/homework160526/Series.java
+++ HomeWork/src/homework160526/Series.java
@@ -2,16 +2,26 @@ package homework160526;
 
 public class Series {
 	public static void main(String[] args) {
-		double a = 1.0;
-		for (int n = 1; n < Integer.MAX_VALUE; n++) {
+		number(0.000005);
+	}
+
+	static double number(double epsilon) {
+		double a = 1;
+		int n = 1;
+		for (; a >= epsilon; n++) {
 			a = 1.0 / ((n + 1) * (n + 1));
+
+			/*double a is out of range */
 			if (a < 0) {
-				System.out.println(n - 1);
-				break;
+				System.out.println(-1);
+				return -1;
 			}
+			
 			System.out.println(a);
 
 		}
+		System.out.println(n - 1);
+		return n - 1;
 	}
 
 }
