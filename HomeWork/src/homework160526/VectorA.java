06e03f8c3955fdb2df8029a2b85cbbcaa4e9e0c5
Elizaveta <Elizaveta@Liza-pc.Dlink>
2016-05-31 09:53:00 +0300

    whith middle element

--- HomeWork/src/homework160526/VectorA.java
+++ HomeWork/src/homework160526/VectorA.java
@@ -2,14 +2,15 @@ package homework160526;
 
 public class VectorA {
 	public static void main(String[] args) {
-		System.out.println(maxsum(0, 2, 5, 8, 9, 4));
+		System.out.println(maxsum(0, 2, 5, 11, 10, 9, 4));
 
 	}
 
 	static double maxsum(double... a) {
 		int len = a.length;
+		int halflen = len % 2 == 0 ? len / 2 : len / 2 + 1;
 		double max = Double.MIN_VALUE;
-		for (int i = 0; i < len / 2; i++) {
+		for (int i = 0; i < halflen; i++) {
 			max = Math.max(a[i] + a[len - 1 - i], max);
 		}
 		return max;
