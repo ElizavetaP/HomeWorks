fe8972a2a045a684ddd25f4e3ea1ab530cf7d3b3
ElizavetaP <ufologia9891>
2016-06-03 18:02:31 +0300

    with hashcode

--- HomeWork/src/homework160531/pen/worker.java
+++ HomeWork/src/homework160531/pen/worker.java
@@ -5,19 +5,24 @@ package homework160531.pen;
  */
 public class worker {
     public static void main(String[] args) {
-        Stationery stationery = new Stationery();
 
         Pen ball = new Pen("black", "ball", 12);
         Pen ball1 = new Pen("red", "ball", 12);
         Pen feather = new Pen("black", "feather", 15);
         Pen feather1 = new Pen("black", "feather", 15);
 
-        stationery.add(ball);
-        stationery.add(ball1);
-        stationery.add(feather);
-        stationery.add(feather1);
 
-        System.out.println(stationery);
+        System.out.println(ball);
+
+        ball.setPrice(100);
+        System.out.println(ball.getPrice());
+
+        System.out.println(ball.equals(ball1));
+        System.out.println(feather.equals(feather1));
+
+
+
+
 
     }
 }
