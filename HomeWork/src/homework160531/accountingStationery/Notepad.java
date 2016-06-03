2ab09e9174737054458f8092a2de72bdb71b0e14
ElizavetaP <ufologia9891>
2016-06-03 19:16:15 +0300

    reformat code

--- HomeWork/src/homework160531/accountingStationery/Notepad.java
+++ HomeWork/src/homework160531/accountingStationery/Notepad.java
@@ -4,6 +4,7 @@ package homework160531.accountingStationery;
  * Created by test on 6/3/16.
  */
 public class Notepad extends OfficeAccessory {
+
     public Notepad(String color, String design, double price) {
         super(color, design, price);
     }
