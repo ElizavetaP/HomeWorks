b35e6f0db3c3324e5142d5a40785902e5b3820b9
ElizavetaP <ufologia9891>
2016-07-20 22:37:13 +0300

    start sort

---
+++ HomeWork/src/algorithms/heapsort/HeapSort.java
@@ -0,0 +1,46 @@
+package algorithms.heapsort;
+
+public class HeapSort {
+    public static void main(String[] args) {
+        int[] array = {1, 4, 7, 3, 5, 9, 44, 2, 90};
+        heapsort(array);
+
+
+    }
+
+    private static void heapsort(int[] array) {
+        int len = array.length;
+        for (int i = 0; i < Math.log(array.length) / Math.log(2) - 1; i++) {
+            tree(array, 0);
+            for (int j : array) {
+                System.out.println(j);
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < len - 2; i++) {
+            tree(array, i);
+            for (int j : array) {
+                System.out.println(j);
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    private static void tree(int[] array, int start) {
+        int len = array.length;
+        for (int i = start; i < (len + start) / 2 + 1; i++) {
+            if ((2 * i + 1 < len) && array[2 * i + 1] > array[i]) {
+                int temp = array[2 * i + 1];
+                array[2 * i + 1] = array[i];
+                array[i] = temp;
+            }
+            if ((2 * i + 2 < len) && array[2 * i + 2] > array[i]) {
+                int temp = array[2 * i + 2];
+                array[2 * i + 2] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+}
